b4ed272972ed321fe94b270ff2b379b9fa4143e3
cwd.systems <101006007+infinitydaemon@users.noreply.github.com>
2023-09-04 17:17:40 +0600

    Create obj_track.py

---
+++ obj_track.py
@@ -0,0 +1,65 @@
+import cv2
+import numpy as np
+
+net = cv2.dnn.readNet("yolov3.cfg", "yolov3.weights")
+
+# Load COCO class names (80 classes)
+with open("coco.names", "r") as f:
+    classes = f.read().strip().split("\n")
+
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = video_capture.read()
+
+    if not ret:
+        break
+
+    # Perform object detection using YOLO
+    blob = cv2.dnn.blobFromImage(frame, 1/255.0, (416, 416), swapRB=True, crop=False)
+    net.setInput(blob)
+    outs = net.forward(net.getUnconnectedOutLayersNames())
+
+    class_ids = []
+    confidences = []
+    boxes = []
+
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+
+            if confidence > 0.5:
+                center_x = int(detection[0] * frame.shape[1])
+                center_y = int(detection[1] * frame.shape[0])
+                width = int(detection[2] * frame.shape[1])
+                height = int(detection[3] * frame.shape[0])
+
+                x = int(center_x - width / 2)
+                y = int(center_y - height / 2)
+
+                class_ids.append(class_id)
+                confidences.append(float(confidence))
+                boxes.append([x, y, width, height])
+
+    indices = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+
+    if len(indices) > 0:
+        for i in indices.flatten():
+            x, y, w, h = boxes[i]
+            label = str(classes[class_ids[i]])
+            confidence = confidences[i]
+            color = (0, 255, 0)  # Green
+
+            cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+            cv2.putText(frame, f"{label} {confidence:.2f}", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+    cv2.imshow("Object Detection", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
+
